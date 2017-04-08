f00ed42d1fb48d3a49d27191f767f84e4fe8db90
Daniel Chrapczynski <daniel.chrapczynski@gmail.com>
2017-04-08 12:19:26 +0000

    Created appointment time validation, delete appointment action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,12 @@
 class AppointmentsController < ApplicationController
+    
     def index
         @appointment = Appointment.all
+        @user_id = @appointment.includes(:user_id)
+    end
+    
+    def destroy 
+        @appointment = Appointment.find(params[:id])
+        
     end
 end
